1baabad2e17fa001ea86005cdc60055fe0aa93df
shchetkin.a.y <shchetkin.a.y@gmail.com>
2022-10-12 11:20:59 +0700

    Finished step 4

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿string[] arr = new string[6]{"1234", "1567", "-2", "computer science", "1", "ABS"};
+string[] result = new string[arr.Length];
+
+void Sercher(string[] arr, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[count] = arr[i];
+            count ++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Sercher(arr, result);
+PrintArray(result);
